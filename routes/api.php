a6c4a334416f5084817e1fc11bc5d2b85b22d0ed
Diaz <owenstokd@gmail.com>
2026-04-09 16:41:42 -0400

    definicion rutas

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function() {
-    Route::get('/ping', fn () => response()->json([
+Route::get('/ping', fn () => response()->json([
         'message' => 'pong',
         'service' => 'Revibo Backend',
         'enviroment' => app()->environment(),
         'timestamp' => now()->toIso8601String()
     ]));
+
+Route::prefix('v1')->group(function() {
+
+    /** 
+     * Archivos de rutas de los módulos bajo versionamiento
+     * http://localhost:8000/api/v1/archivo/test
+     * */ 
+
+
+
+
+
+    require __DIR__.'/Modules/archivoRuta.php';
 });
