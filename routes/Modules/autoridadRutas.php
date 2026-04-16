e50f749200901eadc4298196c0c306774045e61a
Diaz <owenstokd@gmail.com>
2026-04-16 19:08:01 -0400

    rol autorizacion

--- routes/Modules/autoridadRutas.php
+++ routes/Modules/autoridadRutas.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Autoridad\AutoridadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware("auth:sanctum")->prefix("/autoridad")->group(function () {
+Route::middleware(["auth:sanctum","rol:admin"])->prefix("/autoridad")->group(function () {
     Route::get("/", [AutoridadController::class, "index"])->name("autoridad.index");
     Route::post("/", [AutoridadController::class, "store"])->name("autoridad.store");
 });
