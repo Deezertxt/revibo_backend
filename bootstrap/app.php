0e967048a885a13e8588abe278d591462eb6ae02
Diaz <owenstokd@gmail.com>
2026-05-01 15:52:08 -0400

    handle cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'rol' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        
+        // Enable CORS for mobile and web clients
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
