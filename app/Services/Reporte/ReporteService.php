4c5d866e7503290e67a2169f1391d36b07b6fa7b
RomaneQ <quirogaromane2@gmail.com>
2026-05-26 21:53:36 -0400

    reporte

--- app/Services/Reporte/ReporteService.php
+++ app/Services/Reporte/ReporteService.php
@@ -7,32 +7,52 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
 class ReporteService{
+    
+
     public function crear(array $data) {
-        
-        
         $fecha_inicio = $data['fecha_inicio'] ?? now();
         $fecha_fin = $data['fecha_fin'] ?? null;
 
-        return DB::transaction(function() use($data, $fecha_inicio, $fecha_fin){
-            $geoJson = json_encode($data["geom"]);
-            $reporte = Reporte::create(array_merge($data, [
-                "id_reporte" => Str::uuid(),
-                "id_usuario" => request()->user()->id_usuario,
-                "fecha_inicio" => $fecha_inicio, //tambien fecha de creacion
-                "fecha_fin"=> $fecha_fin,
-                "activo" => true
-            ]));
-            if(array_key_exists('url_imagen', $data)){
-                $imagenes = array_map(fn($url) => ['url_imagen' => $url], $data['url_imagen']);
-                $reporte->fotos()->createMany($imagenes);
-            }
-            DB::update("UPDATE reporte SET geom = ST_SetSRID(ST_GeomFromGeoJSON(?), 4326) WHERE id_reporte = ?", [$geoJson, $reporte->id_reporte]);
-            //dd($reporte->all());
-            return $reporte->fresh();
-       });           
+      
+        $geoJson = json_encode($data["geom"]);
+        unset($data["geom"]);
+        
+       
+        $urlImagenes = $data['url_imagen'] ?? [];
+        unset($data['url_imagen']);
+
+       
+        $idReporteNuevo = (string) Str::uuid();
+
+     
+        $reporte = Reporte::create(array_merge($data, [
+            "id_reporte" => $idReporteNuevo,
+            "id_usuario" => request()->user()->id_usuario,
+            "fecha_inicio" => $fecha_inicio, 
+            "fecha_fin"=> $fecha_fin,
+            "activo" => true
+        ]));
+
+     
+        if(!empty($urlImagenes)){
+            $imagenes = array_map(fn($url) => [
+                'id_url_imagen_reporte' => (string) Str::uuid(),
+                'id_reporte'            => $idReporteNuevo,
+                'url_imagen'            => $url,
+                'created_at'            => now(),
+                'updated_at'            => now()
+            ], $urlImagenes);
+            
+            DB::table('url_imagen_reporte')->insert($imagenes);
+        }
+
+        
+        DB::update("UPDATE reporte SET geom = ST_SetSRID(ST_GeomFromGeoJSON(?), 4326) WHERE id_reporte = ?", [$geoJson, $idReporteNuevo]);
+        
+        return $reporte->fresh();
     }
 
     public function actualizar(){
-
+        // ...
     }
 }
